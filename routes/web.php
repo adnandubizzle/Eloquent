8db06e7749f51412811860725ea13170cf0deb3a
Adnan <adnan.umar@dubizzle.com>
2025-07-07 11:25:18 +0500

    experimented With() mehtod and its variations and how it solves n+1 problem

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,12 @@ Route::get('/', function () {
 
 
     //create()
-    //    $author = Author::create( ['name'=> 'Ashfaq Sab'] );
+        //    $author = Author::create( ['name'=> 'Adnan'] );
 
     //make()
-    // $x=Author::make(['name' => 'Don'], ['id'=>100]);
-    // Only saves to DB when  Save() runs otherwise its only in memory and not in DB.
-    // $x->save();d
+        // $x=Author::make(['name' => 'Don'], ['id'=>100]);
+        // Only saves to DB when  Save() runs otherwise its only in memory and not in DB.
+        // $x->save();d
 
     // all()
         // $author=Author::all();
@@ -26,14 +26,45 @@ Route::get('/', function () {
         
     //Select()
         // Author::select('id', 'name')->get(). //Reccomended
-        // Author::get(['id', 'name']); //Not REcommended
+        // Author::get(['id', 'name']); //Not Recommended
 
     //Where()
         // Author::select('name')->where('id','!=', 81)->get()    
 
     
-        //
-    
+    //first()
+        // In SQL its LIMIT 1
+        // Author::select('id','name')->where('name', 'Adnan')->get(). //Return multiple Adnans
+       // Author::select('id','name')->where('name', 'Adnan')->get()->first()     return only first Adnan
+       // first( can also take Arguments like ): Book::where('author_id', 3)->first(['id', 'title']);
+
+
+
+     //find()
+     //filters on based on primary key eg: id
+       //Author::find(82));
+
+    //pluck()  
+        //gives simple data in array(if 1 column arg), or Key Value pair(  "1": "Enrique McKenzie")
+        // You cannot call -> functions on the output as its not returning a model
+        // Author::pluck('name' ,'id')
+
+    //orderBy()
+        //Author::orderBy('created_at')->get():  //By Default Ascending
+        //Author::orderBy('created_at', 'desc')->get():
+
+
+    //count()
+        //Author::get()->count();
+
+
+     //With()
+     //loads table plus forign table in single statement (2 SQL querries) only, Avoids n+1 problem(lazy loading)
+        // Book::with('author')->get()
+
+
+    //with() //with only specifuic cols of forign table
+        // Book::with(['author:id,name'])->get()
 
 
        return response()->json();
